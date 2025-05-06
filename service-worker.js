418207d8c30fa44ff0205f52029da4b44dcda3f3
Rejeki444 <dikiia267@gmail.com>
2025-05-06 09:15:26 +0700

    rezeki
    slot gacor

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+
+const CACHE_NAME = "rejeki444-cache-v1";
+const urlsToCache = [
+  "/index.html",
+  "/assets/style.css",
+  "/assets/jackpot.mp3"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
